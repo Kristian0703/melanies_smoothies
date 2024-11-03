4ae44bee6b59ea3c3970a6f9123e7c4ddeff289b
Kristian0703 <126021674+Kristian0703@users.noreply.github.com>
2024-11-03 11:29:00 +0100

    Update streamlit_app.py
    Refactor the code and make it more legible

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize your smoothie :cup_with_straw:")
@@ -33,7 +34,9 @@ else:
     
         for fruit_choosen in ingredients_list:
             ingredients_string += fruit_choosen + ' '
-    
+            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+            fv_df = st.dataframe(fruityvice_response.json(), use_container_width=True)
+
         my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
                 values ('""" + ingredients_string + """','""" + name_on_order + """')"""
     
@@ -46,8 +49,4 @@ else:
             session.sql(my_insert_stmt).collect()
             st.success(f'Your Smoothie is ordered, {name_on_order}',  icon="✅")
 
-# New section to display Fruityvice nutrition information
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe(fruityvice_response.json(), use_container_width=True)
+
